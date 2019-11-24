f5ea8f60104c92fdf26bd3f0b902edce714825ee
flyingwithgri1 <501174837@qq.com>
2019-11-24 22:18:27 +0800

    部分页面提交

--- src/main/java/com/nyk/demo/MainCtrl.java
+++ src/main/java/com/nyk/demo/MainCtrl.java
@@ -42,4 +42,19 @@ public class MainCtrl {
     public String get3_3_01A(){
         return "3_3_1";
     }
+
+    @RequestMapping("/3_6_1")
+    public String get3_6_01A(){
+        return "3_6_1";
+    }
+
+    @RequestMapping("/3_7_1")
+    public String get3_7_01A(){
+        return "3_7_1";
+    }
+
+    @RequestMapping("/3_8_1")
+    public String get3_8_01A(){
+        return "3_8_1";
+    }
 }
